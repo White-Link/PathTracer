9daf362b53a62a1b98c30823e4d179cfdd98c878
Franceschi <jean-yves.franceschi@ens-lyon.org>
2017-06-16 11:41:49 +0200

    Forgot some code.

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Path Tracer (ENS Lyon -- CR11 -- Mathematical Methods for Image Synthesis -- Project)
 
 ## Author
-
 Jean-Yves Franceschi
 
 ## Files
+ - `cimg` folder: contains `cimg.h`, header file of library CImg for handling image storing.
+ - `src` folder: contains the source files, with:
+   - `main.cpp`: file where the tested scene is defined and the rendering step is launched;
+   - `material.hpp`: defines the materials;
+   - `mesh.hpp` and `mesh.cpp`: implement meshes and textures importation;
+   - `object_container.hpp` and `object_container.cpp`: implement object lists and BVH;
+   - `object.hpp` and `object.cpp`: implement all object types;
+   - `scene.hpp` and `scene.cpp`: implement the scene;
+   - `utils.hpp` and `utils.cpp`: define several useful tools for this project.
+ - `examples` folder: contains several examples of main files and their corresponding result (these are the images produced for the report).
+ - `car`, `lightning` and `triss` folders: contain all three models used in the examples and in the report.
+ - `CMakeLists.txt`: CMake file to configure the project before compilation.
+ - `Doxyfile`: configuration file for Doxygen.
+ - `LICENSE`: MIT license.
+ - `README.md`: this file.
+ - `report.pdf`: report of this project.
 
 ## Dependencies
  - CMake v. 3.1 or above.
@@ -13,3 +28,17 @@ Jean-Yves Franceschi
  - (Optional) Doxygen for a simple documentation of the project.
 
 ## Usage
+
+### General usage
+Given a file `main.cpp` in folder `src`, compilation is done with the following commands in a build folder:
+`cmake [project root folder] && make -j` for Linux, or equivalent commands for another OS.
+
+The executable is created is the project root folder, and is named `path_tracer`.
+
+### Examples
+In order to test one of the examples, one should copy their content to the main file in `src`, and compile again the project.
+
+The program must be executed in the project root tree in order to work correctly (because of relative paths in the examples). However, this constraint does not hold for arbitrary executions of the program with other main files.
+
+### Documentation
+In order to produce the documentation of the code of this project, `make doc` should be executed in the build folder. It produces a `doc` folder in the root tree of the project, containing an HTML (`html/index.html`) and PDF (that should be compiled in folder `latex`) version of the documentation.
--- src/main.cpp
+++ src/main.cpp
@@ -3,39 +3,63 @@
 
 
 int main(int argc, char **argv) {
-	/*Material green = Material(Vector(0,0.7,0.2));
-	Material blue = Material(Vector(0.3,0.1,0.8));
-	Material red = Material(Vector(0.9,0.1,0));
-	Material white = Material(Vector(1,1,1));
-	Material glass = Material(Vector(0.9,0.1,0), 0, 0, true, 1.33);
-	Material red_refract = Material(Vector(0.9,0.1,0), 0, 0, true, 1.8);
-	Material red_mirror = Material(Vector(0.9,0.1,0), 0);
-	Object o1 = Object(Sphere(1, Point(4, 0, 0), white));
-	Object o2 = Object(Plane(Point(0,3,0), Vector(0,3,0), red));
-	Object o3 = Object(Plane(Point(0,-3,0), Vector(0,3,0), blue));
-	Object o4 = Object(Plane(Point(0,0,3), Vector(0,0,2), green));
-	Object o5 = Object(Plane(Point(0,0,-1.5), Vector(0,0,2), blue));
-	Object o6 = Object(Plane(Point(9,0,0), Vector(1,0,0), red));
-	Object o7 = Object(Plane(Point(-3,0,0), Vector(1,0,0), blue));
-	std::vector<Object> objects_v = {o1, o2, o3, o4, o5, o6, o7};*/
-	Material green = Material(Vector(0,0.7,0.2));
-	Material blue = Material(Vector(0.3,0.1,0.8));
-	Material red = Material(Vector(0.9,0.1,0));
-	Material silver = Material(Vector(0.1, 1, 1));
-	Object p1 = Object(Plane(Point(0,0,-0.3), Vector(0,0,1), green));
-	Object p2 = Object(Plane(Point(0,0,5), Vector(0,0,-1), green));
-	Object p3 = Object(Plane(Point(0,-3,0), Vector(0,3,0), blue));
-	Object p4 = Object(Plane(Point(0,3,0), Vector(0,-3,0), blue));
-	Object p5 = Object(Plane(Point(-5,0,0), Vector(1,0,0), red));
-	Object p6 = Object(Plane(Point(2,0,0), Vector(-1,0,0), red));
-	//Mesh mesh("car/sls_amg.obj", "car/", silver);
-	std::vector<Object> objects_v = {/*mesh,*/ p1, p2, p3, p4, p5, p6};
+	Material green = Material(
+		Vector(0,0.7,0.2),
+		Vector(1,1,1),
+		Vector(1,1,1),
+		1,
+		0,
+		100,
+		0,
+		false,
+		1
+	);
+	Material blue = Material(
+		Vector(0.3,0.1,0.8),
+		Vector(1,1,1),
+		Vector(1,1,1),
+		1,
+		0,
+		100,
+		0,
+		false,
+		1
+	);
+	Material red = Material(
+		Vector(0.9,0.1,0),
+		Vector(1,1,1),
+		Vector(1,1,1),
+		1,
+		0,
+		100,
+		0,
+		false,
+		1
+	);
+	Material white = Material(
+		Vector(1,1,1),
+		Vector(1,1,1),
+		Vector(1,1,1),
+		1,
+		0,
+		100,
+		0,
+		false,
+		1
+	);
+	Sphere o1 = Sphere(1, Point(3, 0, 0), white);
+	Plane o2 = Plane(Point(0,3,0), Vector(0,3,0), red);
+	Plane o3 = Plane(Point(0,-3,0), Vector(0,3,0), blue);
+	Plane o4 = Plane(Point(0,0,3), Vector(0,0,2), green);
+	Plane o5 = Plane(Point(0,0,-1.5), Vector(0,0,2), blue);
+	Plane o6 = Plane(Point(9,0,0), Vector(1,0,0), red);
+	Plane o7 = Plane(Point(-3,0,0), Vector(1,0,0), blue);
+	std::vector<Object> objects_v = {o1, o2, o3, o4, o5, o6, o7};
 	ObjectVector objects(objects_v.begin(), objects_v.end());
-	//Camera camera(Point(0,0,0), Vector(1,0,0), Vector(0,0,1), 60*PI/180, 900, 1600);
-	Camera camera(Point(-1.5,0,1), Vector(2,0,-1), Vector(1,0,2), 60*PI/180, 900, 1600);
+	Camera camera(Point(-1,0,0), Vector(1,0,0), Vector(0,0,1), 60*PI/180, 1000, 1000);
 	Scene scene(camera, objects);
-	scene.AddLight(Light(Point(-3, 0, 3), Vector(40, 40, 40)));
-	scene.Render(10, 200, true, false);
+	scene.AddLight(Light(Point(-2, -1, 2), Vector(50, 50, 50)));
+	scene.Render(10, 50, true, false);
 	scene.Save("test.bmp");
 	return 0;
 }
